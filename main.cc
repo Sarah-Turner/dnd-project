f935d47e82817cbf82aadad7832c43c2d36d5491
Damian Turner <damian.ronald.turner@gmail.com>
2021-03-27 15:59:12 -0700

    Fix extra endl

--- main.cc
+++ main.cc
@@ -2,10 +2,10 @@
 int main() {
     char response;
     std::cout << "1. Create Enemy Type" << std::endl;
-    std::cout << "2. List Created Types" << std::endlendl;
-    std::cout << "3. Generate Enemy From Type" << std::endlendl;
-    std::cout << "4. List Generated Enemies" << std::endlendl;
-    std::cout << "5. Exit" << std::endlendl;
+    std::cout << "2. List Created Types" << std::endl;
+    std::cout << "3. Generate Enemy From Type" << std::endl;
+    std::cout << "4. List Generated Enemies" << std::endl;
+    std::cout << "5. Exit" << std::endl;
     std::cout << "Enter Choice: ";
     std::cin >> response;
 
