66c01756233a7f2a1ef60ece01e642a2826ba137
Sarah Turner <smt01@live.com>
2021-03-27 15:21:34 -0700

    Added Main Menu output

--- main.cc
+++ main.cc
@@ -1,3 +1,16 @@
 int main() {
+    char response;
+    std::cout << "1. Create Enemy Type" << endl;
+    std::cout << "2. List Created Types" << endl;
+    std::cout << "3. Generate Enemy From Type" << endl;
+    std::cout << "4. List Generated Enemies" << endl;
+    std::cout << "5. Exit" << endl;
+    std::cout << "Enter Choice: ";
+    std::cin >> response;
+
+    switch (response)
+    {
+        
+    }
     return 0;
 }
