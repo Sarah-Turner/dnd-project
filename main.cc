6e113f3ed782bb19398577fb289abc4f3016edf9
Damian Turner <damian.ronald.turner@gmail.com>
2021-03-27 16:21:56 -0700

    Add shell of program to switch between menus

--- main.cc
+++ main.cc
@@ -1,17 +1,49 @@
 #include <iostream>
-int main() {
-    char response;
-    std::cout << "1. Create Enemy Type" << std::endl;
-    std::cout << "2. List Created Types" << std::endl;
-    std::cout << "3. Generate Enemy From Type" << std::endl;
-    std::cout << "4. List Generated Enemies" << std::endl;
-    std::cout << "5. Exit" << std::endl;
-    std::cout << "Enter Choice: ";
-    std::cin >> response;
 
-    switch (response)
-    {
-        
-    }
+void CreateEnemyType() {
+
+}
+
+void ListCreatedTypes() {
+
+}
+
+void GenerateEnemy() {
+
+}
+
+void ListEnemies() {
+
+}
+
+int main() {
+    char response = 0;
+    while (response != 5) {
+        std::cout << "1. Create Enemy Type" << std::endl;
+        std::cout << "2. List Created Types" << std::endl;
+        std::cout << "3. Generate Enemy From Type" << std::endl;
+        std::cout << "4. List Generated Enemies" << std::endl;
+        std::cout << "5. Exit" << std::endl;
+        std::cout << "Enter Choice: ";
+        std::cin >> response;
+        switch (response)
+        {
+            case 1:
+                CreateEnemyType();
+                break; 
+            case 2:
+                ListCreatedTypes();
+                break;
+            case 3:
+                GenerateEnemy();
+                break;
+            case 4:
+                ListEnemies();
+                break;
+            default:
+                break;
+        }
+    } 
+    std::cout << "Goodbye!" std::endl;
     return 0;
 }
