b64a5f747f438c03a8c16ef1bdf35615c2fc188d
Sarah Turner <smt01@live.com>
2021-03-27 15:32:13 -0700

    Added loop for menu output

--- main.cc
+++ main.cc
@@ -1,12 +1,15 @@
 int main() {
     char response;
-    std::cout << "1. Create Enemy Type" << endl;
-    std::cout << "2. List Created Types" << endl;
-    std::cout << "3. Generate Enemy From Type" << endl;
-    std::cout << "4. List Generated Enemies" << endl;
-    std::cout << "5. Exit" << endl;
-    std::cout << "Enter Choice: ";
-    std::cin >> response;
+    do
+    {
+        std::cout << "1. Create Enemy Type" << endl;
+        std::cout << "2. List Created Types" << endl;
+        std::cout << "3. Generate Enemy From Type" << endl;
+        std::cout << "4. List Generated Enemies" << endl;
+        std::cout << "5. Exit" << endl;
+        std::cout << "Enter Choice: ";
+        std::cin >> response;
+    } while (response < 1 || response > 5)
 
     switch (response)
     {
