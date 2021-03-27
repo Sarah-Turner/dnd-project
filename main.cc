fb8ea0ffcecb05784c6fab13f2f39fb47167b01d
Damian Turner <damian.ronald.turner@gmail.com>
2021-03-27 15:58:41 -0700

    Add std:: to endl calls

--- main.cc
+++ main.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 int main() {
     char response;
-    std::cout << "1. Create Enemy Type" << endl;
-    std::cout << "2. List Created Types" << endl;
-    std::cout << "3. Generate Enemy From Type" << endl;
-    std::cout << "4. List Generated Enemies" << endl;
-    std::cout << "5. Exit" << endl;
+    std::cout << "1. Create Enemy Type" << std::endl;
+    std::cout << "2. List Created Types" << std::endlendl;
+    std::cout << "3. Generate Enemy From Type" << std::endlendl;
+    std::cout << "4. List Generated Enemies" << std::endlendl;
+    std::cout << "5. Exit" << std::endlendl;
     std::cout << "Enter Choice: ";
     std::cin >> response;
 
