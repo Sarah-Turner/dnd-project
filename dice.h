7e0ed86e3502d4426d8024b25e18122679836f88
Damian Turner <damian.ronald.turner@gmail.com>
2021-03-27 15:44:06 -0700

    Add a dice roll function to generate random number

---
+++ dice.h
@@ -0,0 +1,5 @@
+#include <random>
+namespace sarah {
+    // Generates a random number from a dice with num_sides sides. Generator should be re-used for all rolls.
+    int Roll(const std::default_random_engine& generator, int num_sides);
+}
